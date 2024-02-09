6e118b509fe8fcf5e453c402308119804ff42f33
jsonch <jsonch@seas.upenn.edu>
2024-02-09 14:29:36 -0500

    single threaded portknock with by-value atom functions

---
+++ examples/portknock_c2/portknock.c
@@ -0,0 +1,255 @@
+/*
+    compile: gcc -Wall -g -o portknock portknock.c -lpcap
+    run: ./portknock <pcap file> 
+
+    portknock_c with atoms with value args / returns 
+    instead of pointers.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include <netinet/if_ether.h>  // for struct ether_header (Ethernet header)
+// #include <netinet/ip.h>        // for struct iphdr (IP header)
+// #include <netinet/tcp.h>       // for struct tcphdr (TCP header)
+
+/* inline header structs (for macos compatability) */
+#define ETH_P_IP    0x0800      /* Internet Protocol packet */
+#define ETHER_ADDR_LEN  6   /* length of an Ethernet address */
+struct  ether_header {
+    u_int8_t  ether_dhost[ETHER_ADDR_LEN];
+    u_int8_t  ether_shost[ETHER_ADDR_LEN];
+    u_int16_t ether_type;
+} __attribute__((__packed__));
+
+struct iphdr {
+    uint8_t  ihl    :4,
+         version:4;
+    uint8_t   tos;
+    uint16_t  tot_len;
+    uint16_t  id;
+    uint16_t  frag_off;
+    uint8_t   ttl;
+    uint8_t   protocol;
+    uint16_t  check;
+    int32_t   saddr;
+    int32_t   daddr;
+};
+typedef        u_int32_t tcp_seq;
+
+struct tcphdr {
+    uint16_t  source;
+    uint16_t  dest;
+    uint32_t  seq;
+    uint32_t  ack_seq;
+    uint16_t   res1:4,
+        doff:4,
+        fin:1,
+        syn:1,
+        rst:1,
+        psh:1,
+        ack:1,
+        urg:1,
+        ece:1,
+        cwr:1;
+    uint16_t  window;
+    uint16_t check;
+    uint16_t  urg_ptr;
+};
+
+
+#include <pcap.h>              // for pcap functions
+
+
+/*** static definitions used to structure user-written code ***/
+
+/* the program must define a "metadata" struct, 
+   which contains the data extracted from the packet.*/
+struct metadata; 
+
+
+/*** static library functions ***/
+int main(int argc, char *argv[]);
+void handle_packet(struct pcap_pkthdr *header, const u_char *packet);
+void ip_to_string(uint32_t ip, char *buf);
+void print_tcp_pkt(const u_char *pkt, int len);
+
+// global state helpers
+#define CREATE_STATE(state_name, state_type, state_size) \
+    state_type state_name[state_size] = {0}
+#define GET_STATE(state_name, state_type, index) \
+    &((state_type *)state_name)[index]
+
+
+/*** user/compiler written code ***/
+
+// atom structures
+typedef struct flow_key_t {
+    uint8_t  valid; // does every atom io type have to have a valid field? If not, how do we handle errors? 
+    uint32_t src_ip;
+    uint32_t dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+} flow_key_t;
+
+
+// parse extracts the 
+flow_key_t parse(const u_char *pkt, uint32_t pkt_len) {
+    flow_key_t rv = {0};
+    // exit if packet is too small
+    if (pkt_len < sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct tcphdr)) {
+        return rv;
+    }
+    struct ether_header *eth = (struct ether_header *)pkt;
+    // exit if packet is not IP
+    if (eth->ether_type != htons(ETH_P_IP)) {
+        return rv;
+    }
+    struct iphdr *ip = (struct iphdr *)(pkt + sizeof(struct ether_header));
+    // exit if packet is not TCP
+    if (ip->protocol != IPPROTO_TCP) {
+        return rv;
+    }
+    struct tcphdr *tcp = (struct tcphdr *)(pkt + sizeof(struct ether_header) + sizeof(struct iphdr));
+    // parsing success -- fill in metadata
+    rv.src_ip = ip->saddr;
+    rv.dst_ip = ip->daddr;
+    rv.src_port = ntohs(tcp->source);
+    rv.dst_port = ntohs(tcp->dest);
+    rv.valid = 1;
+}
+
+// state transition atom
+typedef enum {
+  CLOSED_0 = 0,
+  CLOSED_1,
+  CLOSED_2,
+  OPEN,
+} state_t;
+#define PORT_1 100
+#define PORT_2 101
+#define PORT_3 102
+
+typedef struct {
+    uint8_t valid; 
+    state_t final_state;   
+} update_result_t;
+
+
+// state for update
+typedef struct array_elem {
+    uint32_t state;
+} array_elem;
+
+CREATE_STATE(port_states, array_elem, 1024);
+update_result_t update(const u_char *pkt, flow_key_t fk) {
+    update_result_t rv  = {0};
+    // get pointer to current state for the port    
+    array_elem *value = GET_STATE(port_states, array_elem, 0);
+    if (!value) {return rv;} // null pointer -- stop processing.
+    // update the state
+    if (value->state == OPEN) {
+        // no update necessary
+    } else if (value->state == CLOSED_0 && fk.dst_port == PORT_1) {
+        value->state = CLOSED_1;
+    } else if (value->state == CLOSED_1 && fk.dst_port == PORT_2) {
+        value->state = CLOSED_2;
+    } else if (value->state == CLOSED_2 && fk.dst_port == PORT_3) {
+        value->state = OPEN;
+    } else {
+        value->state = CLOSED_0;
+    }
+    // fill in the metadata field for cur_state
+    rv.valid = 1;
+    rv.final_state = value->state;
+    return rv;        
+}
+
+// action atom -- swap ethernet addresses
+typedef enum {
+  DROP = 0,
+  PASS
+} decision_t;
+
+decision_t action(const u_char *pkt, uint32_t pkt_len, update_result_t res) {
+    // swap ethernet addresses
+    if (pkt_len < sizeof(struct ether_header)) {
+        return DROP;
+    }
+    struct ether_header *eth = (struct ether_header *)pkt;
+    // only swap if final state is OPEN
+    if (res.final_state == OPEN) {
+        uint8_t tmp[6];
+        memcpy(tmp, eth->ether_shost, 6);
+        memcpy(eth->ether_shost, eth->ether_dhost, 6);
+        memcpy(eth->ether_dhost, tmp, 6);
+    }
+    return PASS;
+}
+
+
+/* packet handling function. just a chain of functions */
+void handle_packet(struct pcap_pkthdr *header, const u_char *packet) {
+    printf ("---initial packet---\n");
+    print_tcp_pkt(packet, header->len);
+    flow_key_t parse_out = parse(packet, header->len);
+    if (!parse_out.valid) {return;}
+    update_result_t update_out = update(packet, parse_out);
+    if (!update_out.valid) {return;}
+    decision_t action_out = action(packet, header->len, update_out);
+    printf("final packet---\n");
+    print_tcp_pkt(packet, header->len);
+    printf("final state: %d\n", update_out.final_state);
+    printf("action: %d\n", action_out);
+}
+
+
+void ip_to_string(uint32_t ip, char *buf) {
+    // use inet function to convert ip address uint32_t to string
+    struct in_addr addr;
+    addr.s_addr = ip;
+    char *ip_str = inet_ntoa(addr);
+    sprintf(buf, "%s", ip_str);
+}
+
+// print the ethernet addresses, ip addresses, and ports of a tcp packet
+void print_tcp_pkt(const u_char *pkt, int len) {
+    struct ether_header *eth = (struct ether_header *)pkt;
+    struct iphdr *ip = (struct iphdr *)(pkt + sizeof(struct ether_header));
+    struct tcphdr *tcp = (struct tcphdr *)(pkt + sizeof(struct ether_header) + sizeof(struct iphdr));
+    char *src_ip_str = malloc(16); ip_to_string(ip->saddr, src_ip_str);
+    char *dst_ip_str = malloc(16); ip_to_string(ip->daddr, dst_ip_str);
+    printf("src_mac: %02x:%02x:%02x:%02x:%02x:%02x, dst_mac: %02x:%02x:%02x:%02x:%02x:%02x, src_ip: %s, dst_ip: %s, src_port: %d, dst_port: %d\n", 
+        eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2], eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5],
+        eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2], eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5],
+        src_ip_str, dst_ip_str, ntohs(tcp->source), ntohs(tcp->dest));
+    free(src_ip_str); free(dst_ip_str);
+}
+
+
+
+/* main loop -- open pcap, read files */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = pcap_open_offline(argv[1], errbuf);
+    if (handle == NULL) {
+        printf("Couldn't open pcap file %s: %s\n", argv[1], errbuf);
+        return 1;
+    }
+
+    struct pcap_pkthdr header;
+    const u_char *packet;
+
+    while ((packet = pcap_next(handle, &header)) != NULL) {
+        handle_packet(&header, packet);
+    }
+
+    pcap_close(handle);
+    return 0;
+}
---
+++ examples/portknock_c2/testutils.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+# this script contains utilities for generating test packets and traces.
+
+import sys, time, random, socket, os, struct, json, copy
+import binascii
+import dpkt
+from collections import namedtuple
+
+"""Testspec utilities."""
+usage = "usage: testspec_info.py testspec.json pkts.pcap"
+def main():
+    generatePcap(sys.argv[1], sys.argv[2])
+
+defaultPktRec = {
+    "eth.src":"12:12:12:12:12:12",
+    "eth.dst":"34:34:34:34:34:34",
+    "eth.type":dpkt.ethernet.ETH_TYPE_IP,
+    "ip.src" : "12.12.12.12",
+    "ip.dst" : "34.34.34.34",
+    "ip.id"  : 0,
+    "ip.tos" : 0,
+    "ip.ttl" : 64,
+    "ip.proto" : "tcp",
+    "tcp.src" : 10,
+    "tcp.dst" : 20,
+    "payload.length" : 128
+}
+def generatePcap(testspecfn, pcapFn):
+    # parse trace json file.
+    traceJson = json.load(open(testspecfn))
+    templateRecs = traceJson["packet_templates"]
+    # load the template packet records
+    for (name, templateRec) in templateRecs.items():
+        # grab defaults for any fields that are not filled in. 
+        rec = {field:templateRec.get(field, defaultPktRec[field]) for field in defaultPktRec.keys()}
+        templateRecs[name].update(rec)
+    # print("--- packet templates ---")
+    # for (name, templateRec) in templateRecs.items():
+    #     print ("[%s] %s" % (name, templateRec))
+    specRecs = traceJson["packets"]
+    packetRecs = []
+    for specRec in specRecs:
+        default_rec = copy.deepcopy(defaultPktRec)
+        # use the template as base, if there is one
+        if (specRec["template"]):
+            default_rec.update(templateRecs[specRec["template"]])
+        # update the defaults with the spec
+        rec = {field:specRec.get(field, default_rec[field]) for field in default_rec.keys()}
+        packetRecs.append(rec)
+    pkts = [pktFromRec(p) for p in packetRecs]
+
+    # write the packets to the output pcap file.
+    pw = PcapWriter()
+    pw.create(pcapFn)
+    for (i, pkt) in enumerate(pkts):
+        pw.write(pkt, i*4) # packets are 4 seconds apart
+    pw.close()
+
+
+def fmt_ipaddr(addr):
+    if (isinstance(addr, str)):
+        return socket.inet_aton(addr)
+    elif (isinstance(addr, int)):
+        return struct.pack('!L', addr)
+
+def pktFromRec(rec):
+    """ convert a pktRec into a packet """
+    # craft a L4 packet (tcp / udp)
+    L4Packet = None
+    if (rec["eth.type"] == dpkt.ethernet.ETH_TYPE_IP):
+        if (rec["ip.proto"] == "udp"):
+            payload = b'X'*int(rec["payload.length"])
+            udpPkt = dpkt.udp.UDP(
+                sport = int(rec["udp.src"]), 
+                dport = int(rec["udp.dst"]))
+            udpPkt.data = payload
+            udpPkt.ulen += len(udpPkt.data)
+            L4Packet = bytes(udpPkt)
+        elif (rec["ip.proto"] == "tcp"):
+            payload = b'X'*int(rec["payload.length"])
+            tcpPkt = dpkt.tcp.TCP(
+                sport = int(rec["tcp.src"]), 
+                dport = int(rec["tcp.dst"]),
+                data = payload
+            )
+            tcpPkt.sum = 0  # let dpkt compute the checksum
+            L4Packet = bytes(tcpPkt)
+
+        else:
+            print ("ERROR: NON TCP/UDP PACKETS NOT IMPLEMENTED")
+            quit()
+
+    L3Packet = None
+
+    # craft a L3 packet (ip)
+    if (rec["eth.type"] == dpkt.ethernet.ETH_TYPE_IP):
+        src = None
+        proto = rec["ip.proto"]
+        if (proto == "tcp"):
+            proto = dpkt.ip.IP_PROTO_TCP
+        elif (proto == "udp"):
+            proto = dpkt.ip.IP_PROTO_UDP
+
+        ipPkt = dpkt.ip.IP(v=4, 
+            id = int(rec["ip.id"]), 
+            src = fmt_ipaddr(rec["ip.src"]), 
+            dst = fmt_ipaddr(rec["ip.dst"]), 
+            p   = int(proto),
+            tos = int(rec["ip.tos"]), 
+            ttl = int(rec["ip.ttl"])
+        )
+        ipPkt.data = L4Packet
+        ipPkt.len += len(L4Packet)
+        L3Packet = bytes(ipPkt)
+    else:
+        print ("ERROR: NON IP PACKETS NOT IMPLEMENTED")
+        quit()
+
+    # craft a L2 frame (ethernet)
+    hexStrToBin = lambda ethAddr : binascii.unhexlify(ethAddr.replace(":", ""))
+    L2Frame = None
+    ethFrame = dpkt.ethernet.Ethernet(
+        src=hexStrToBin(rec["eth.src"]), 
+        dst=hexStrToBin(rec["eth.dst"]), 
+        type=int(rec["eth.type"])
+    )
+    ethFrame.data = L3Packet
+    L2Frame = bytes(ethFrame)    
+    return L2Frame
+
+
+class PcapWriter(object):
+    """ Write bytestring packets into a PCAP file."""
+    # global header for a version 2.4 PCAP file.
+    pcap_global_header=bytes.fromhex('d4c3b2a1') + struct.pack("H",2) + struct.pack("H",4) + struct.pack("I", 0) + struct.pack("I", 0) + struct.pack("I", 1600) + struct.pack("I", 1)
+    def __init__(self, f = None):
+        if (f):
+            self.f = f
+            self.f.write(self.pcap_global_header)            
+        return
+
+    def create(self, filename):
+        self.f = open(filename, "wb")
+        self.f.write(self.pcap_global_header)
+
+    def write(self, pkt, tsSec):
+        """
+        Writes an ethernet packet to a file. Prepends the pcap packet header.
+        """
+        pcap_len = len(pkt)
+        seconds = int(tsSec)
+        microseconds = int((tsSec - int(tsSec)) * 1000000)
+        pktBytes = struct.pack("<i",seconds) + struct.pack("<i",microseconds) + struct.pack("<i", pcap_len) + struct.pack("<i", pcap_len) + bytes(pkt)
+        self.f.write(pktBytes)
+
+    def close(self):
+        self.f.close()
+
+
+
+
+if __name__ == '__main__':
+    main()
